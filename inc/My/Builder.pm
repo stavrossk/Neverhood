6f12538a8ff142eadfe7820f5b78eb6a220773ea
Blaise Roth <blaiseroth@gmail.com>
2012-12-25 05:14:18 -0800

    Removed uninstall from My Builder
    It wasn't very useful

--- inc/My/Builder.pm
+++ inc/My/Builder.pm
@@ -4,96 +4,7 @@ use strict;
 use warnings;
 use base 'Module::Build';
 
-# So SDL doesn't go crazy on us when we require Games::Neverhood
-$ENV{SDL_VIDEODRIVER} = 'dummy';
-$ENV{SDL_AUDIODRIVER} = 'dummy';
-
 =head1 ACTIONS
-
-=item uninstall
-
-[version 0.03] (Blaise Roth, experimental)
-
-This action will find the .packlist file made when the distribution was
-installed and attempt to delete all files listed in it. The process of
-finding the .packlist file requires the module to be installed, so you
-will need to install the distribution again to reattempt an uninstall.
-
-WARNING: This operation is potentially dangerous if you have newlines in
-your path to Perl's lib directory. As a precaution, if there are any
-newlines found in any of the strings in C<@INC>, this command will
-refuse to run. If you are sure there are no newlines messing up the
-.packlist file then you can force the command to continue anyway by
-specifying --force option.
-
-=back
-
-=cut
-
-sub ACTION_uninstall {
-	require Games::Neverhood;
-	$@ and leave("Games::Neverhood wouldn't load: $@Maybe install before uninstalling?");
-	require File::ShareDir;
-	require File::Spec;
-	my $dir = File::ShareDir::module_dir('Games::Neverhood');
-	my $packlist = File::Spec->catfile($dir, '.packlist');
-
-	unless(grep defined && $_ eq 'force', $_[0]->args()) {
-		for(@INC) {
-			leave(sprintf(<<'WARNING', $packlist)) if /\n/;
-This operation is potentially dangerous because you have newlines in a
-path to Perl's lib directory (strings in @INC). This could mess up the
-.packlist file used by this action to uninstall the distribution. The
-distribution's .packlist file is located at:
-%s
-
-If you are sure there are no newlines messing up the .packlist file then
-you can force the command to continue anyway by specifying the --force
-option.
-WARNING
-		}
-	}
-
-	use autodie ':all';
-	open LIST, ">>", $packlist; #Just makin' sure we can write in it later
-	open LIST, "<", $packlist;
-	my $leftover;
-	my $total = my $deleted = 0;
-	say "Deleting all files listed in $packlist";
-	while(<LIST>) {
-		chomp;
-		no autodie;
-		if(unlink) {
-			$deleted++;
-		}
-		elsif(-e) {
-			say STDERR "Couldn't delete $_: $!";
-			$leftover .= "$_\n";
-		}
-		else {
-			$total--;
-		}
-		$total++;
-	}
-	if(defined $leftover and $deleted) {
-		say "$deleted of $total files successfully deleted";
-		say "Updating .packlist with remaining files";
-		open LIST, ">", $packlist;
-		print LIST $leftover;
-		say ".packlist updated with remaining files";
-	}
-	else {
-		say "all files successfully deleted";
-		if(do { no autodie; unlink $packlist }) {
-			say ".packlist deleted";
-		}
-		else {
-			open LIST, ">", $packlist;
-			say ".packlist emptied";
-		}
-	}
-	close LIST;
-}
 
 =item license
 
@@ -119,11 +30,6 @@ sub ACTION_license {
 	open LICENSE, ">", File::Spec->catfile('LICENSE');
 	print LICENSE $license->fulltext();
 	say "LICENSE file created successfully";
-}
-
-sub leave {
-	say STDERR $_[0];
-	exit;
 }
 
 1;
